9c10472e2b14a45a5cbd51f8d452eb20a5e90224
Hoon2e <49215105+Hoon2e@users.noreply.github.com>
2022-12-14 17:23:52 +0900

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    setTimeout(() => {throw new Error('hi')},3000)
     return 'Hello World22!';
   }
 }
